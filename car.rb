e385a131699e87c9a41703bdec01888a2b15ba9a
ArvinLleva <arvin.lleva@yahoo.com>
2019-10-24 18:18:28 -0700

    what I have done once class finished

--- car.rb
+++ car.rb
@@ -1,2 +1,50 @@
 class Vehicle
+    def initialize(model_year)
+        @model_year = model_year
+    end
+
+    def model_year
+        @model_year
+    end
+end
+
+class Car
+    def initialize
+        @wheels = 4
+    end
+
+    def wheels
+        @wheels
+    end
+
+    def honk_horn
+        'BEEP!'
+    end
+
+    def model_year
+        @model_year
+    end
+end
+
+class Toyota < Car
+    def initialize
+        super()
+    end
+
+    def honk_horn
+        'whoop'
+    end
+end
+
+
+class Tata
+    def honk_horn
+        "beep"
+    end
+end
+
+class Tesla
+    def honk_horn
+        "Beep-bee-bee-boop-bee-doo-weep"
+    end
 end
--- car_spec.rb
+++ car_spec.rb
@@ -2,7 +2,84 @@ require 'rspec'
 require_relative 'car'
 
 describe "Vehicle" do
-  it "has to be real" do
-    expect{Vehicle.new}.to_not raise_error
+  it "Vehicle class has to exist" do
+    expect{Vehicle.new(1990)}.to_not raise_error
+  end
+
+  context "when having the vehicle year model" do
+    it "vehicle class can be initialized with the year model" do 
+      my_vehicle = Vehicle.new(1990)
+      expect(my_vehicle.model_year).to eq(1990)
+    end
+  end
+end
+
+
+describe "Car" do
+  # let allows us to create a set of data that can be used in a group
+  let(:my_car) { Car.new }
+
+
+  it "Car class has to exist" do
+    expect{my_car}.to_not raise_error
+  end
+
+
+  it "default class has default variable wheels to be 4" do
+    expect(my_car.wheels).to eq(4)
+  end
+
+  context "using the honk_horn method" do
+    it "should print out 'BEEP!'" do
+      expect(my_car.honk_horn).to eq('BEEP!')       
+    end
+  end
+end
+
+describe "Toyota" do
+  let(:my_toyota) { Toyota.new}
+
+  it "Toyota class exists" do 
+    expect{my_toyota}.to_not raise_error
+  end
+
+  it "Toyota inherits from class Car" do
+    expect(Toyota.superclass).to be Car
+  end
+
+  context "using the honk_horn for toyota" do
+    it "should return 'whoop'" do
+      expect(my_toyota.honk_horn).to eq("whoop")
+    end
+  end
+
+end
+
+
+describe "Tata" do
+  let(:my_tata) { Tata.new }
+
+  it "Tata class has to exist" do
+    expect{my_tata}.to_not raise_error
+  end
+
+  context "using the honk_horn method" do
+    it "should print out 'BEEP!'" do
+      expect(my_tata.honk_horn).to eq('beep')       
+    end
+  end
+end
+
+describe "Tesla" do
+  let(:my_tesla) { Tesla.new }
+
+  it "Tesla class has to exist" do
+    expect{my_tesla}.to_not raise_error
+  end
+
+  context "using the honk_horn method" do
+    it "should print out 'BEEP!'" do
+      expect(my_tesla.honk_horn).to eq('Beep-bee-bee-boop-bee-doo-weep')       
+    end
   end
 end
